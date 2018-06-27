08588de33d475b7fa09c7abd9bf85cabb92341b5
peterquintas <peter@quintas.org>
2018-06-27 00:08:45 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ fi
 
 if [ -n "$CRON_TIME" ]; then
   echo "${CRON_TIME} /backup.sh >> /dockup.log 2>&1" > /crontab.conf
-  env | grep 'AWS\|BACKUP_NAME\|S3_BUCKET_NAME|PATHS_TO_BACKUP' | cat - /crontab.conf > temp && mv temp /crontab.conf
+  env | grep 'AWS\|BACKUP_NAME\|S3_BUCKET_NAME\|PATHS_TO_BACKUP' | cat - /crontab.conf > temp && mv temp /crontab.conf
   crontab  /crontab.conf
   echo "=> Running dockup backups as a cronjob for ${CRON_TIME}"
   exec cron -f
